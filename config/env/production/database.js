ccec5854820d8eda8c079b52a8c1284949929ed7
Adi Nugroho <addieze@ymail.com>
2022-12-17 15:53:01 +0700

    change database details

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", "5432"),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      database: env("DATABASE_NAME", "db-mapel-peminatan"),
+      user: env("DATABASE_USERNAME", "adiinugr"),
+      password: env("DATABASE_PASSWORD", "Shar1ngan"),
       ssl: {
         ca: env("DATABASE_CA"),
       },
